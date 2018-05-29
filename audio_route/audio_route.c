53f5f39d3019381f63cc3146ba98e446c32a91a6
Ramu Gottipati <ramug@codeaurora.org>
2018-05-29 19:07:21 +0530

    audio_route: fix free_path memory leak
    This change will fix memory leak in audio_route.
    
    Change-Id: I40e1297e4aebdcd815596c93db14b8126f172689
    Signed-off-by: panshuangquan <panshuangquan@xiaomi.com>
    Git-commit: 5d8a6541928dce89a68211e80ea4c432485ffd20
    Git-repo: https://android.googlesource.com/platform/system/media/+/5d8a6541928dce89a68211e80ea4c432485ffd20%5E%21
    
    Change-Id: Ic1f482985ac77d3c936f751c1ed3278a52dbdc08

--- audio_route/audio_route.c
+++ audio_route/audio_route.c
@@ -115,14 +115,20 @@ static void __attribute__((unused)) path_print(struct audio_route *ar, struct mi
 static void path_free(struct audio_route *ar)
 {
     unsigned int i;
+    size_t j;
 
     for (i = 0; i < ar->num_mixer_paths; i++) {
         if (ar->mixer_path[i].name)
             free(ar->mixer_path[i].name);
         if (ar->mixer_path[i].setting) {
             if (ar->mixer_path[i].setting->value)
-                free(ar->mixer_path[i].setting->value);
+                for (j = 0; j < ar->mixer_path[i].length; j++) {
+                     free(ar->mixer_path[i].setting[j].value);
+                }
             free(ar->mixer_path[i].setting);
+            ar->mixer_path[i].size = 0;
+            ar->mixer_path[i].length = 0;
+            ar->mixer_path[i].setting = NULL;
         }
     }
     free(ar->mixer_path);
@@ -394,11 +400,17 @@ static void start_tag(void *data, const XML_Char *tag_name,
             if (state->level == 1) {
                 /* top level path: create and stash the path */
                 state->path = path_create(ar, (char *)attr_name);
+                if (state->path == NULL)
+                    ALOGE("path created failed, please check the path if existed");
             } else {
                 /* nested path */
                 struct mixer_path *sub_path = path_get_by_name(ar, attr_name);
-                path_add_path(ar, state->path, sub_path);
-            }
+                if (!sub_path) {
+                    ALOGE("unable to find sub path '%s'", attr_name);
+                } else if (state->path != NULL) {
+                    path_add_path(ar, state->path, sub_path);
+                }
+           }
         }
     }
 
@@ -457,7 +469,8 @@ static void start_tag(void *data, const XML_Char *tag_name,
                 mixer_value.index = atoi((char *)attr_id);
             else
                 mixer_value.index = -1;
-            path_add_value(ar, state->path, &mixer_value);
+            if (state->path != NULL)
+                path_add_value(ar, state->path, &mixer_value);
         }
     }
 
